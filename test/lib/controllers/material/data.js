7c291e3468fd2c51a266fa0fe6284945e851f162
Justin Merz <jrmerz@gmail.com>
2015-12-11 16:33:10 -0800

    more tests, bug fixes for bubbling recalc errors

--- test/lib/controllers/material/data.js
+++ test/lib/controllers/material/data.js
@@ -10,7 +10,63 @@ simple1 : {
 simple2 : {
   name : 'fuel',
   price : 3.75,
-  units : '[us_gal]'
+  units : '[gal_us]'
+},
+
+complex1 : {
+  name : 'truck',
+  units : 'h',
+  materials : {
+    labor : {
+      amount : 1,
+      units : 'h'
+    },
+    fuel : {
+      amount : 3,
+      units : '[gal_us]'
+    }
+  }
+},
+
+complex2 : {
+  name : 'truck and trailer',
+  units : 'h',
+  materials : {
+    truck : {
+      amount : 1,
+      units : 'h'
+    },
+    fuel : {
+      amount : 4,
+      units : 'L'
+    }
+  }
+},
+
+complex3 : {
+  name : 'cycle badness',
+  units : 'h',
+  materials : {
+    'labor' : {
+      amount : 4,
+      units : 'h'
+    },
+    'cycle badness2' : {
+      amount : 4,
+      units : 'h'
+    }
+  }
+},
+
+complex4 : {
+  name : 'cycle badness2',
+  units : 'h',
+  materials : {
+    'cycle badness' : {
+      amount : 1,
+      units : 'h'
+    }
+  }
 }
 
 
--- test/lib/controllers/material/index.js
+++ test/lib/controllers/material/index.js
@@ -13,7 +13,7 @@ describe('Material Controller', function() {
     var response = controller.add(data.simple1);
 
     assert.equal(response.success, true);
-    assert.equal(controller.get('labor').name, 'labor');
+    assert.equal(controller.get(data.simple1.name).name, 'labor');
   });
 
   it('fire event on add', function(done){
@@ -47,13 +47,68 @@ describe('Material Controller', function() {
     controller.reset();
 
     controller.add(data.simple1);
-    assert.equal(controller.get('labor').price, 1);
+    assert.equal(controller.get(data.simple1.name).price, 1);
 
     var update = require('./data')().simple1;
     update.price = 2;
     var response = controller.add(update, {replace: true});
 
     assert.equal(response.success, true);
-    assert.equal(controller.get('labor').price, 2);
+    assert.equal(controller.get(data.simple1.name).price, 2);
+  });
+
+  it('calculate complex materials', function(){
+    controller.reset();
+
+    controller.add(data.simple1);
+    controller.add(data.simple2);
+
+    // add complex1
+    var response = controller.add(data.complex1);
+    assert.equal(response.success, true);
+
+    var m = controller.get(data.complex1.name);
+    assert.equal(m.price, 12.25);
+
+    // add complex 2
+    response = controller.add(data.complex2);
+    assert.equal(response.success, true);
+
+    var m = controller.get(data.complex2.name);
+    // complex to is complex1 + 4 more liters of fuel.
+    // so 4 * fuel price (3.75) * gal to liter (0.264172) = 3.96258;
+    // 3.96258 + 12.25 = 16.21258
+    assert.equal(m.price.toFixed(5), '16.21258');
+  });
+
+  it('should complain with missing required material', function(){
+    controller.reset();
+    controller.add(data.complex1);
+    var m = controller.get(data.complex1.name);
+    assert.equal(m.recalcErrors.length, 2);
+  });
+
+  it('should not allow cyclical complex material dependencies', function(){
+    controller.reset();
+    controller.add(data.simple1);
+
+    controller.add(data.complex3);
+    controller.add(data.complex4);
+
+    assert.equal(data.complex3.recalcErrors.length, 1);
+    assert.equal(data.complex4.recalcErrors.length, 1);
+  });
+
+  it('should let me fix last test cyclical errors', function(){
+    var complex3 = controller.get(data.complex3.name);
+    var complex4 = controller.get(data.complex4.name);
+
+    // we should be able to fix these errors as well;
+    delete complex3.materials[complex4.name];
+    controller.add(complex3, {replace: true});
+
+    assert.equal(complex3.recalcErrors.length, 0);
+    assert.equal(complex4.recalcErrors.length, 0);
+    assert.equal(complex4.price, 4);
   });
 })
