e3df3dca5cd088792efc8151f6d2f75c1c39c415
Justin Merz <jrmerz@gmail.com>
2016-01-20 12:21:56 -0800

    updates, bug fixes

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,29 @@
 
 var saveTimer = -1;
 
+function UiBuffer(fn, timeout, bind) {
+  var timer = -1;
+
+  function bufferFn() {
+    if( timer !== -1 ) {
+      clearTimeout(timer);
+    }
+    var args = arguments;
+
+    timer = setTimeout(function(){
+      timer = -1;
+      fn.apply(bind, args);
+    }, timeout);
+  }
+
+  return bufferFn;
+}
+
 SDK.app = {
+
+  // buffers function (fn) by timeout in ms.  Binds function to whatever bind is provided.
+  UiBuffer : UiBuffer,
+
   getMaterialLink : function(name) {
     return '#budget/materials/'+encodeURIComponent(name);
   },
@@ -34,10 +56,26 @@ SDK.app = {
     return true;
   },
 
+  cloneBudget : function() {
+    if( SDK.changes.hasChanges() && !confirm('Are you sure you want to clone this budget and save as new?') ) return;
+
+    var data = {
+      budget : SDK.getBudget().data,
+      materials : SDK.controllers.material.asArray()
+    }
+    data.budget.name = '';
+    data.budget.id = SDK.utils.guid();
+
+    window.location = '#budget/overview';
+
+    document.querySelector('ahb-budget').load(data);
+    document.querySelector('budget-farm-editor').show(true);
+  },
+
   saveBudget : function(callback) {
     SDK.app.localsave();
 
-    var budget = $.extend(true,{}, SDK.getBudget());
+    var budget = $.extend(true,{}, SDK.getBudget().data);
     SDK.utils.strip(SDK.schema.budget, budget);
 
     budget.materialIds = [];
@@ -73,7 +111,7 @@ SDK.app = {
   _localsave : function(auto) {
     var t = new Date().getTime();
     var data = {
-      budget : SDK.getBudget(),
+      budget : SDK.getBudget().data,
       materials : []
     }
 
@@ -92,14 +130,86 @@ SDK.app = {
   }
 };
 
+// privately wire update changes stuff
+(function(){
+
+  // this we will want to buffer
+  function checkForChanges() {
+    SDK.changes.checkBudget(SDK.getBudget().data, SDK.controllers.material.asArray());
+  }
+
+  function onDiffCheckComplete(diff) {
+    console.log('budget---');
+    console.log(diff);
+    // path in new event.  This one buffered and what the UI cares about.
+    if( diff.budget.length > 0 || diff.materialIds.length > 0 ) {
+      SDK.changes.getEventsModule().emit('save-state-update', {
+        changes: true,
+        budget : diff.budget.length > 0 ? true : false,
+        materials : diff.materialIds > 0 ? true : false
+      });
+      return;
+    }
+
+    SDK.changes.getEventsModule().emit('save-state-update', {changes: false});
+  }
+
+  // set the app api call, but with the buffered function.  this call isn't cheap
+  var buffer = new SDK.app.UiBuffer(checkForChanges, 200);
+  SDK.app.checkForChanges = buffer;
+  SDK.app.checkForChangesNow = checkForChanges;
+
+  SDK.changes.on('budget-diff', onDiffCheckComplete);
+
+
+  //
+  // now wire up stuff for material changes
+  //
+  function checkForMaterialChanges() {
+    SDK.changes.checkBudget(SDK.getBudget().data, SDK.controllers.material.asArray());
+  }
+
+  function onMaterialDiffComplete(diff) {
+    console.log('materials---');
+    console.log(diff);
+
+    // TODO: why am I checking the unsaved changes array here?
+    if( diff.length > 0 || Object.keys(SDK.changes.getUnsaved()).length ) {
+      SDK.changes.getEventsModule().emit('material-state-update', {changes: true});
+    } else {
+      SDK.changes.getEventsModule().emit('material-state-update', {changes: false});
+    }
+  }
+  buffer = new SDK.app.UiBuffer(checkForMaterialChanges, 200);
+  SDK.app.checkForMaterialChanges = buffer;
+  SDK.changes.on('material-diff', onMaterialDiffComplete);
+})();
+
 
 // listen to events and save
+SDK.controllers.material.on('material-added', function(){
+  SDK.app.localsave();
+  SDK.app.checkForChanges();
+  SDK.app.checkForMaterialChanges();
+});
 SDK.controllers.material.on('material-update', function(){
   SDK.app.localsave();
+  SDK.app.checkForChanges();
+  SDK.app.checkForMaterialChanges();
 });
-SDK.controllers.material.on('material-removed', SDK.app.localsave);
-SDK.controllers.operation.on('operation-removed', SDK.app.localsave);
-SDK.controllers.operation.on('operation-update', SDK.app.localsave);
+SDK.controllers.material.on('material-removed', function(){
+  SDK.app.localsave();
+  SDK.app.checkForChanges();
+  SDK.app.checkForMaterialChanges();
+});
+SDK.controllers.operation.on('operation-removed', function(){
+  SDK.app.localsave();
+  SDK.app.checkForChanges();
+});
+SDK.controllers.operation.on('operation-update', function(){
+  SDK.app.localsave();
+  SDK.app.checkForChanges();
+});
 
 // save every 30 seconds
 setInterval(function(){
