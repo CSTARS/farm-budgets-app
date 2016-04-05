f26efc6ae891f6f587e9f9b4e5d7d24b81ff8e2d
Justin Merz <jrmerz@gmail.com>
2016-04-04 17:58:03 -0700

    nightly checkin

--- public/js/app.js
+++ public/js/app.js
@@ -20,29 +20,10 @@ function UiBuffer(fn, timeout, bind) {
   return bufferFn;
 }
 
-$(window).on('hashchange', function() {
-  SDK.app.history = SDK.app._history;
-  SDK.app._history = window.location.hash;
-});
-
 SDK.app = {
 
   // buffers function (fn) by timeout in ms.  Binds function to whatever bind is provided.
   UiBuffer : UiBuffer,
-
-  history : null,
-  _history : window.location.hash,
-
-  _init : false,
-  init : function() {
-    if( this._init ) return;
-    this._init = true;
-
-    // save every 30 seconds
-    setInterval(function(){
-      SDK.app.localsave(true);
-    }, 1000*30);
-  },
 
   login : function() {
     window.localStorage.setItem('login-redirect', window.location.pathname+window.location.hash);
@@ -91,151 +72,87 @@ SDK.app = {
   },
 
   cloneBudget : function() {
-    if( SDK.changes.hasChanges() && !confirm('Are you sure you want to clone this budget and save as new?') ) return;
-
     var data = {
       budget : SDK.getBudget().getData(),
       materials : SDK.controllers.material.asArray()
     }
+
     data.budget.name = '';
     data.budget.id = SDK.utils.guid();
 
-    window.location = '#budget/overview';
+    SDK.app.creating = true;
+    SDK.setBudget(data);
 
-    document.querySelector('ahb-budget').load(data);
+    window.location = '#'+data.budget.id;
     document.querySelector('budget-farm-editor').show(true);
   },
 
-  saveBudget : function(callback) {
-    SDK.app.localsave();
+  createNew : function() {
+    var newData = {
+      budget : {
+        id : SDK.utils.guid(),
+        authority : ExpressAuth.user ? ExpressAuth.user.email : '',
+        locality : [],
+        name : '',
+        commodity : '',
+        farm : {
+          name : '',
+          units : '[acr_us]',
+          size : 1
+        },
+        operations : [],
+      },
+      materials : []
+    };
 
+    SDK.app.creating = true;
+    SDK.setBudget(newData);
+
+    window.location = '#'+newData.budget.id;
+    document.querySelector('budget-farm-editor').show(true);
+  }
+};
+
+(function(){
+  function autoSave() {
     var budget = $.extend(true,{}, SDK.getBudget().getData());
-    SDK.utils.strip(SDK.schema().budget, budget);
 
-    budget.materialIds = [];
-
-    var materials = SDK.controllers.material.get();
-    for( var name in materials ) {
-      if( !materials[name].id ) continue;
-      budget.materialIds.push(materials[name].id);
+    if( !SDK.app.hasAccess(budget.budget) ) {
+      document.querySelector('ahb-budget').showWarning();
+      return;
     }
 
+    SDK.emit('auto-save-start');
+    SDK.utils.strip(SDK.schema().budget, budget);
+
     SDK.budgets.save(budget, function(resp){
-      if( callback ) callback(resp);
+      SDK.emit('auto-save-stop', resp);
 
       if( resp.error ) {
         return alert(resp.message);
       }
 
-      // the server may have cleanup some stuff
-      // true flag forces reload without prompt
-      document.querySelector('ahb-budget').reload(budget.id, true);
+      SDK.app.creating = false;
     }.bind(this));
-  },
-
-  localsave : function(auto) {
-    if( saveTimer !== -1 ) clearTimeout(saveTimer);
-
-    saveTimer = setTimeout(function(){
-      saveTimer = -1;
-      SDK.app._localsave(auto);
-    }, 100);
-  },
-
-  _localsave : function(auto) {
-    var t = new Date().getTime();
-    var data = {
-      budget : SDK.getBudget().getData(),
-      materials : []
-    }
-
-    if( !data.budget.id ) return;
-
-    var materials = SDK.controllers.material.get();
-    for( var key in materials ) {
-      data.materials.push(materials[key]);
-    }
-
-    window.localStorage.setItem('current-budget', data.budget.id);
-    window.localStorage.setItem(data.budget.id, JSON.stringify(data));
-    SDK.app.localdata = data;
-
-    console.log('Local '+(auto ? 'auto ' : '') +'save: '+(new Date().getTime() - t)+'ms');
-  }
-};
-
-// privately wire update changes stuff
-(function(){
-
-  // this we will want to buffer
-  function checkForChanges() {
-    SDK.changes.checkBudget(SDK.getBudget().getData(), SDK.controllers.material.asArray());
   }
 
-  function onDiffCheckComplete(diff) {
-    // path in new event.  This one buffered and what the UI cares about.
-    if( diff.budget.length > 0 || diff.materialIds.length > 0 ) {
-      SDK.changes.getEventsModule().emit('save-state-update', {
-        changes: true,
-        budget : diff.budget.length > 0 ? true : false,
-        materials : diff.materialIds > 0 ? true : false
-      });
-      return;
-    }
-
-    SDK.changes.getEventsModule().emit('save-state-update', {changes: false});
-  }
-
-  // set the app api call, but with the buffered function.  this call isn't cheap
-  var buffer = new SDK.app.UiBuffer(checkForChanges, 200);
-  SDK.app.checkForChanges = buffer;
-  SDK.app.checkForChangesNow = checkForChanges;
-
-  SDK.changes.on('budget-diff', onDiffCheckComplete);
-
-
-  //
-  // now wire up stuff for material changes
-  //
-  function checkForMaterialChanges() {
-    SDK.changes.checkBudget(SDK.getBudget().getData(), SDK.controllers.material.asArray());
-  }
-
-  function onMaterialDiffComplete(diff) {
-    // TODO: why am I checking the unsaved changes array here?
-    if( diff.length > 0 || Object.keys(SDK.changes.getUnsaved()).length ) {
-      SDK.changes.getEventsModule().emit('material-state-update', {changes: true});
-    } else {
-      SDK.changes.getEventsModule().emit('material-state-update', {changes: false});
-    }
-  }
-  buffer = new SDK.app.UiBuffer(checkForMaterialChanges, 200);
-  SDK.app.checkForMaterialChanges = buffer;
-  SDK.changes.on('material-diff', onMaterialDiffComplete);
+  SDK.app.autoSave = new UiBuffer(autoSave, 1000);
 })();
 
 
 // listen to events and save
 SDK.controllers.material.on('material-added', function(){
-  SDK.app.localsave();
-  SDK.app.checkForChanges();
-  SDK.app.checkForMaterialChanges();
+  SDK.app.autoSave();
 });
 SDK.controllers.material.on('material-update', function(){
-  SDK.app.localsave();
-  SDK.app.checkForChanges();
-  SDK.app.checkForMaterialChanges();
+  SDK.app.autoSave();
 });
 SDK.controllers.material.on('material-removed', function(){
-  SDK.app.localsave();
-  SDK.app.checkForChanges();
-  SDK.app.checkForMaterialChanges();
+  SDK.app.autoSave();
 });
 SDK.controllers.operation.on('operation-removed', function(){
-  SDK.app.localsave();
-  SDK.app.checkForChanges();
+  SDK.app.autoSave();
 });
 SDK.controllers.operation.on('operation-update', function(){
-  SDK.app.localsave();
-  SDK.app.checkForChanges();
+  SDK.app.autoSave();
 });
